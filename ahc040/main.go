c2c113b8d98326b2f24fae2dfcb757a2501b9a88
fmhr <fmhr29@gmail.com>
2024-12-03 15:22:46 +0900

    wit

--- ahc040/main.go
+++ ahc040/main.go
@@ -288,20 +288,143 @@ func checkEstimate(in Input) {
 	sumErrp := 0.0
 	for i := 0; i < in.N; i++ {
 		fmt.Scan(&trueSize[i][0], &trueSize[i][1])
-		log.Printf("%dw, true:%v, estimate:%v, errp=%.4f%%\n", i, trueSize[i][0], estimate[i][0], float64(absInt(trueSize[i][0]-estimate[i][0]))/float64(trueSize[i][0]))
-		log.Printf("%dh, true:%v, estimate:%v, errp=%.4f%%\n", i, trueSize[i][1], estimate[i][1], float64(absInt(trueSize[i][1]-estimate[i][1]))/float64(trueSize[i][1]))
+		log.Printf("%dw, true:%v, estimate:%v, err:%d errp=%.4f%%\n", i, trueSize[i][0], estimate[i][0], estimate[i][0]-trueSize[i][0], float64(absInt(trueSize[i][0]-estimate[i][0]))/float64(trueSize[i][0]))
+		log.Printf("%dh, true:%v, estimate:%v, err:%d errp=%.4f%%\n", i, trueSize[i][1], estimate[i][1], estimate[i][1]-trueSize[i][1], float64(absInt(trueSize[i][1]-estimate[i][1]))/float64(trueSize[i][1]))
 		sumErrp += float64(absInt(trueSize[i][0]-estimate[i][0]))/float64(trueSize[i][0]) + float64(absInt(trueSize[i][1]-estimate[i][1]))/float64(trueSize[i][1])
 		sumErr += absInt(trueSize[i][0]-estimate[i][0]) + absInt(trueSize[i][1]-estimate[i][1])
+
 	}
 	log.Printf("sumErr:%.4f%% avgErr:%.4f%%\n", sumErrp, (sumErrp / float64(in.N*2)))
 	log.Printf("sumErr:%d avgErr:%.4f\n", sumErr, float64(sumErr)/float64(in.N*2))
+}
+
+type EstimateValue struct {
+	mesuredCnt [2]int     // 0:w, 1:h
+	mesureSum  [2]float64 // 測定したときの結果の合計
+	partyCnt   [2][]int   // 他の長方形が一緒に測定した回数
+}
+
+// estimaterはin.T-1回までqueryを使って推定する
+func estimater(in Input) {
+	estimateV := make([][2]float64, in.N)
+	for i := 0; i < in.N; i++ {
+		estimateV[i][0] = float64(in.w[i])
+		estimateV[i][1] = float64(in.h[i])
+	}
+	puts := make([][]byte, 0)
+	var results [][2]float64
+	for t := 0; t < in.T; t++ {
+		put := make([]byte, in.N)
+		for i := 0; i < in.N; i++ {
+			// それぞれの長方形をw, hのどちらかに配置する
+			put[i] = "UL"[rand.Intn(2)]
+		}
+		fmt.Println(in.N)
+		for i := 0; i < in.N; i++ {
+			//log.Printf("%d %d %s %d\n", i, 0, string(put[i]), -1)
+			fmt.Printf("%d %d %s %d\n", i, 0, string(put[i]), -1)
+		}
+		var w, h float64
+		fmt.Scan(&w, &h)
+		results = append(results, [2]float64{w, h})
+		puts = append(puts, put)
+	}
+	// 推定
+	// 長方形のw,hを順番に推定する
+	// 上の測定回数をまず数える
+	estise := make([]EstimateValue, in.N)
+	for i := 0; i < in.N; i++ {
+		estise[i].partyCnt[0] = make([]int, in.N)
+		estise[i].partyCnt[1] = make([]int, in.N)
+	}
+	for k, p := range puts {
+		// k回目の測定結果
+		for i, d := range p {
+			// 0だけは常に両方測定される
+			if d == 'U' || i == 0 {
+				estise[i].mesuredCnt[0]++
+				estise[i].mesureSum[0] += results[k][0]
+				party := slicesIndex(p, 'U') // 一緒に測定された長方形の番号
+				for _, j := range party {
+					estise[i].partyCnt[0][j]++
+				}
+			}
+			// 0だけは常に両方測定されるのでelse if はつかわない
+			if d == 'L' || i == 0 {
+				estise[i].mesuredCnt[1]++
+				estise[i].mesureSum[1] += results[k][1]
+				party := slicesIndex(p, 'L')
+				for _, j := range party {
+					estise[i].partyCnt[1][j]++
+				}
+			}
+		}
+	}
+	log.Println(estise[0].partyCnt[1])
+	// 0だけは常に両方測定される
+	estise[0].mesuredCnt[0] = len(puts)
+	estise[0].mesuredCnt[1] = len(puts)
+	for i := 0; i < in.N; i++ {
+		estise[i].partyCnt[0][0] = len(puts) // 0だけは常に両方測定される
+		estise[i].partyCnt[1][0] = len(puts)
+		estise[i].partyCnt[0][i] = 0 // 自分自身は加算しない 更新するので
+		estise[i].partyCnt[1][i] = 0
+	}
+	// 例 x番目のwを推定する
+	//log.Printf("estise %d %.2f\n", estise[x].mesuredCnt[wh], estise[x].mesureSum[wh])
+	//log.Println("partyCnt", estise[x].partyCnt[wh])
+	for i := 0; i < in.N; i++ {
+		for wh := 0; wh < 2; wh++ {
+			//log.Println(i, wh, estise[i].partyCnt[wh])
+		}
+	}
+	maxStep := 50000
+	burnIn := 1000
+	sigma := in.sgm
+	sigma = 1000
+	var samplese [100][2][]float64
+	for step := 0; step < maxStep; step++ {
+		for x := 0; x < in.N; x++ {
+			for wh := 0; wh < 2; wh++ {
+				//log.Printf("estise %d %.2f\n", estise[x].mesuredCnt[wh], estise[x].mesureSum[wh])
+				//log.Println("partyCnt", estise[x].partyCnt[wh], x, wh)
+				value := estise[x].mesureSum[wh] // 0番目のwの測定結果の合計
+				// valueから他の長方形のwを引く
+				for i := 0; i < in.N; i++ {
+					// i番目とx番目が一緒に測定された回数 * 推定値
+					value -= float64(estise[x].partyCnt[wh][i]) * estimateV[i][wh]
+				}
+				mean := value / float64(estise[x].mesuredCnt[wh]) // 0番目のwの参加回数を引いて平均を取る
+				if mean < 0 {
+					mean = estimateV[x][wh]
+				}
+				new := rand.NormFloat64()*float64(sigma)/math.Sqrt(2.0) + mean
+				//log.Println(x, wh, estimateV[x][wh], mean, new)
+				if step == maxStep-1 {
+					//log.Printf("%d %d %.2f %.2f %.2f\n", x, wh, estimateV[x][wh], mean, new)
+				}
+				estimateV[x][wh] = math.Max(0, new)
+				samplese[x][wh] = append(samplese[x][wh], estimateV[x][wh])
+			}
+		}
+	}
+	for i := 0; i < in.N; i++ {
+		for wh := 0; wh < 2; wh++ {
+			mean := mean(samplese[i][wh][burnIn:])
+			std := std(samplese[i][wh][burnIn:])
+			log.Println(i, wh, int(mean), int(std))
+		}
+	}
+	//estimateV[x][wh] = new
+	// estimateW[0] = int(new) // 更新
 }
 
 func main() {
 	log.SetFlags(log.Lshortfile)
 	startTIme := time.Now()
 	in := input()
-	solver(in)
+	//solver(in)
+	estimater(in)
 	elap := time.Since(startTIme)
 	log.Printf("time_ms=%d ms\n", elap.Milliseconds())
 	checkEstimate(in)
@@ -326,4 +449,32 @@ func absInt(a int) int {
 		return -a
 	}
 	return a
+}
+
+func slicesIndex(slices []byte, v byte) (indexes []int) {
+	for i, s := range slices {
+		if s == v {
+			indexes = append(indexes, i)
+		}
+	}
+	return
+}
+
+// 平均値を計算
+func mean(samples []float64) float64 {
+	sum := 0.0
+	for _, v := range samples {
+		sum += v
+	}
+	return sum / float64(len(samples))
+}
+
+// 標準偏差を計算
+func std(samples []float64) float64 {
+	m := mean(samples)
+	var sum float64
+	for _, v := range samples {
+		sum += (v - m) * (v - m)
+	}
+	return math.Sqrt(sum / float64(len(samples)))
 }
