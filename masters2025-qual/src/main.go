9c8bc7fbf75c500b8408ece274fed6a45425ce83
fmhr <fmhr29@gmail.com>
2025-03-02 13:22:36 +0900

    add files

--- masters2025-qual/src/main.go
+++ masters2025-qual/src/main.go
@@ -24,12 +24,65 @@ func main() {
 	frand = rand.New(rand.NewSource(1))
 	startTime = time.Now()
 	in := readInput()
+	ans := solver(in)
+	fmt.Println(ans)
 	log.Printf("in: %+v\n", in)
+}
 
+type State struct {
+	grid  [GridSize * GridSize]byte
+	pos   Pos
+	score int
+}
+
+func (s *State) Do(a Action) {
+	if s.grid[s.pos.y][s.pos.x] == '.' {
+		s.score++
+	}
+}
+
+type Pos struct {
+	y, x int
+}
+
+type Act int
+
+const (
+	Move  Act = 1
+	Carry Act = 2
+	Roll  Act = 3
+)
+
+var acts = []Act{Move, Carry, Roll}
+
+type Direction int
+
+const (
+	Up    Direction = 1
+	Down  Direction = 2
+	Left  Direction = 3
+	Right Direction = 4
+)
+
+var directions = []Direction{Up, Down, Left, Right}
+
+type Action struct {
+	act  Act
+	dict Direction
+}
+
+const GridSize = 20
+
+func solver(in In) (ans string) {
+	for i := 0; i < in.N; i++ {
+		log.Println(string(in.grid[i*GridSize : i*GridSize+in.N]))
+	}
+	return ans
 }
 
 type In struct {
-	N int
+	N, M int
+	grid [GridSize * GridSize]byte
 }
 
 func readInput() In {
@@ -37,9 +90,19 @@ func readInput() In {
 	writer := bufio.NewWriter(os.Stdout)
 	defer writer.Flush()
 	var in In
-	_, err := fmt.Fscan(reader, &in.N)
+	_, err := fmt.Fscan(reader, &in.N, &in.M)
 	if err != nil {
 		log.Fatal(err)
+	}
+	for i := 0; i < in.N; i++ {
+		var line string
+		_, err := fmt.Fscan(reader, &line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for j, c := range line {
+			in.grid[i*GridSize+j] = byte(c)
+		}
 	}
 	return in
 }
