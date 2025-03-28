1231dc4942670100de23957d522b5f6f04a06969
fmhr <fmhr29@gmail.com>
2025-03-29 02:28:40 +0900

    add UF

--- ahc045/src/main.go
+++ ahc045/src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sort"
 )
 
 func init() {
@@ -12,7 +13,51 @@ func init() {
 func main() {
 	log.Println("Hello, World!")
 	in := readInput()
-	log.Println(in.M, in.L, in.W)
+	log.Printf("M=%d L=%d W=%d\n", in.M, in.L, in.W)
+	solver(in)
+}
+
+type Point struct {
+	Y, X int
+}
+
+type City struct {
+	Point
+	ID   int
+	area [4]int
+}
+
+func solver(in Input) {
+	var cities [N]City
+	for i := 0; i < N; i++ {
+		cities[i].ID = i
+		cities[i].Y = (in.lxrxlyry[i*4+2] + in.lxrxlyry[i*4+3]) / 2
+		cities[i].X = (in.lxrxlyry[i*4+0] + in.lxrxlyry[i*4+1]) / 2
+		cities[i].area[0] = in.lxrxlyry[i*4+0]
+		cities[i].area[1] = in.lxrxlyry[i*4+1]
+		cities[i].area[2] = in.lxrxlyry[i*4+2]
+		cities[i].area[3] = in.lxrxlyry[i*4+3]
+	}
+	sortedCity := cities[:]
+	sort.Slice(sortedCity, func(i, j int) bool {
+		if sortedCity[i].Y == sortedCity[j].Y {
+			return sortedCity[i].X < sortedCity[j].X
+		}
+		return sortedCity[i].Y < sortedCity[j].Y
+	})
+	gropes := make([][]int, in.M)
+	index := 0
+	for i := 0; i < in.M; i++ {
+		gropes[i] = make([]int, in.G[i])
+		for j := 0; j < in.G[i]; j++ {
+			gropes[i][j] = sortedCity[index].ID
+			index++
+		}
+		log.Println(i, in.G[i], gropes[i])
+	}
+	// クエリの終了
+	fmt.Println("!")
+
 }
 
 const (
@@ -40,4 +85,34 @@ func readInput() (in Input) {
 		fmt.Scan(&in.lxrxlyry[i])
 	}
 	return in
+}
+
+type UnionFind struct {
+	parent []int // 親ノードのインデックス
+}
+
+func NewUnionFind(n int) *UnionFind {
+	uf := &UnionFind{
+		parent: make([]int, n),
+	}
+	for i := 0; i < n; i++ {
+		uf.parent[i] = i
+	}
+	return uf
+}
+func (uf *UnionFind) Find(x int) int {
+	if uf.parent[x] != x {
+		uf.parent[x] = uf.Find(uf.parent[x])
+	}
+	return uf.parent[x]
+}
+func (uf *UnionFind) Union(x, y int) {
+	rootX := uf.Find(x)
+	rootY := uf.Find(y)
+	if rootX != rootY {
+		uf.parent[rootY] = rootX
+	}
+}
+func (uf *UnionFind) Same(x, y int) bool {
+	return uf.Find(x) == uf.Find(y)
 }
